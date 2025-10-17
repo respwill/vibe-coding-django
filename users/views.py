1d538a35e638fdf5557bdc0b028d48e3b5034ee3
Jayden <gonchogo@hotmail.com>
2025-10-17 16:00:17 +0900

    사용자 인증 기능 구현: 회원가입, 로그인, 로그아웃 뷰/폼/템플릿 및 URL 연결, settings.py 템플릿 경로 추가

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+	if request.method == 'POST':
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			user = form.get_user()
+			login(request, user)
+			return redirect('home')
+		else:
+			messages.error(request, '로그인 정보가 올바르지 않습니다.')
+	else:
+		form = AuthenticationForm()
+	return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+	logout(request)
+	return redirect('login')
+
+def register_view(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			messages.success(request, '회원가입이 완료되었습니다. 로그인해 주세요.')
+			return redirect('login')
+		else:
+			messages.error(request, '회원가입 정보가 올바르지 않습니다.')
+	else:
+		form = UserCreationForm()
+	return render(request, 'users/register.html', {'form': form})
