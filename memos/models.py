62ad16e12bd01e767253fbd517dc45c030dd7a18
Jayden <gonchogo@hotmail.com>
2025-10-17 16:01:50 +0900

    메모 CRUD 기능 구현: Memo 모델, 폼, 뷰, 템플릿 및 URL 연결

--- memos/models.py
+++ memos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Memo(models.Model):
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
 
 # Create your models here.
--- memos/views.py
+++ memos/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Memo
+from .forms import MemoForm
 
-# Create your views here.
+@login_required
+def memo_list(request):
+	memos = Memo.objects.filter(author=request.user).order_by('-created_at')
+	return render(request, 'memos/memo_list.html', {'memos': memos})
+
+@login_required
+def memo_detail(request, pk):
+	memo = get_object_or_404(Memo, pk=pk, author=request.user)
+	return render(request, 'memos/memo_detail.html', {'memo': memo})
+
+@login_required
+def memo_create(request):
+	if request.method == 'POST':
+		form = MemoForm(request.POST)
+		if form.is_valid():
+			memo = form.save(commit=False)
+			memo.author = request.user
+			memo.save()
+			return redirect('memo_detail', pk=memo.pk)
+	else:
+		form = MemoForm()
+	return render(request, 'memos/memo_form.html', {'form': form})
+
+@login_required
+def memo_edit(request, pk):
+	memo = get_object_or_404(Memo, pk=pk, author=request.user)
+	if request.method == 'POST':
+		form = MemoForm(request.POST, instance=memo)
+		if form.is_valid():
+			form.save()
+			return redirect('memo_detail', pk=memo.pk)
+	else:
+		form = MemoForm(instance=memo)
+	return render(request, 'memos/memo_form.html', {'form': form})
+
+@login_required
+def memo_delete(request, pk):
+	memo = get_object_or_404(Memo, pk=pk, author=request.user)
+	if request.method == 'POST':
+		memo.delete()
+		return redirect('memo_list')
+	return render(request, 'memos/memo_confirm_delete.html', {'memo': memo})
